0cae5f700ad699efd371d26372dc1ed7215095ed
dohoonkim <dohoonkim023@gmail.com>
2023-10-24 16:56:52 +0900

    minor change

--- src/test/kotlin/com/side/alarmsystem/alarm/adapter/out/persistence/AlarmJpaEntity.kt
+++ src/test/kotlin/com/side/alarmsystem/alarm/adapter/out/persistence/AlarmJpaEntity.kt
@@ -3,7 +3,6 @@ package com.side.alarmsystem.alarm.adapter.out.persistence
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
-import lombok.NoArgsConstructor
 
 @Entity
 class AlarmJpaEntity(
