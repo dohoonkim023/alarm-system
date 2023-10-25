232d55ae204b698d547ef9035c45b88e9343e2a0
dohoonkim <dohoonkim023@gmail.com>
2023-10-25 22:23:28 +0900

    minor change

--- src/main/kotlin/com/side/alarmsystem/alarm/adapter/in/web/SendAlarmController.kt
+++ src/main/kotlin/com/side/alarmsystem/alarm/adapter/in/web/SendAlarmController.kt
@@ -1,7 +1,13 @@
 package com.side.alarmsystem.alarm.adapter.`in`.web
 
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class SendAlarmController {
+
+    @PostMapping("/alarm")
+    fun sendAlarm(): String {
+        return "sendAlarm"
+    }
 }
